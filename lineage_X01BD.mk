dc480475b9b4d6879d7809e0746e368bdbae38e9
ManitnjG <102397396+ManitnjG@users.noreply.github.com>
2024-04-29 11:41:13 +0530

    Update and rename rising_X01BD.mk to lineage_X01BD.mk

--- lineage_X01BD.mk
+++ lineage_X01BD.mk
@@ -10,7 +10,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
 # Inherit some common PixelOS stuff
-$(call inherit-product, vendor/rising/config/rising.mk)
+$(call inherit-product, vendor/lineage/config/common-full-phone.mk)
 
 # Inherit from X01BD device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -22,7 +22,7 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_CALL_RECORDING_SUPPORTED := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := rising_X01BD
+PRODUCT_NAME := lineage_X01BD
 PRODUCT_DEVICE := X01BD
 PRODUCT_BRAND := Asus
 PRODUCT_MODEL := Zenfone Max Pro M2
